ff0817d8b25defad2ce2c4f7c5f4b416232e0cf3
yosepdwik <yosepdwikristanto@usd.ac.id>
2024-12-08 12:36:59 +0100

    Update index

--- toba_lake.R
+++ toba_lake.R
@@ -25,7 +25,29 @@ matrix_toba <- resize_matrix(matrix_toba, scale = .15)
 
 matrix_toba |> 
   sphere_shade() |> 
-  plot_3d(matrix_toba, zscale = 12)
+  add_water(detect_water(matrix_toba), color = "desert") |> 
+  add_shadow(ray_shade(matrix_toba), .5) |> 
+  add_shadow(ambient_shade(matrix_toba), 0) |> 
+  plot_map()
+matrix_toba |> 
+  sphere_shade() |> 
+  add_water(detect_water(matrix_toba), color = "desert") |> 
+  add_shadow(ray_shade(matrix_toba), .5) |> 
+  add_shadow(ambient_shade(matrix_toba), 0) |> 
+  plot_3d(
+    matrix_toba, zscale = 17,
+    fov = 0, theta = 135, zoom = .75, phi = 45,
+    windowsize = c(800, 800),
+    background = "#BCF2F6",
+    soil = TRUE,
+    soil_color_dark = "#593a0e",
+    soil_color_light = "#72601b"
+  )
+render_clouds(
+  matrix_toba, zscale = 17, start_altitude = 1000, end_altitude = 1500,
+  attenuation_coef = 2, clear_clouds = TRUE, cloud_cover = .5
+)
+render_snapshot()
 save_3dprint(
   filename = "toba_lake.stl",
   maxwidth = 15,
